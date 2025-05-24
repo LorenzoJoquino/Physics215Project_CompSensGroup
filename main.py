7fbe1eec79fb370cbde615a96c2070064788c0bc
LorenzoJoquino <lpjoquino@up.edu.ph>
2025-05-25 07:45:17 +0800

    can run main.py in a website

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from fasthtml.common import *
-import numpy as np
 
-app, rt = fast_app()
+app,rt = fast_app()
 
-@rt
-def get():
-    return Div(P("Hello World"), hx_get="/change")
+@rt('/')
+def get(): return Div(P('Hello World!'), hx_get="/change")
 
 serve()
