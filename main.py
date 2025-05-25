e239b54d25a4c38d104222439f053f08bfd010de
LorenzoJoquino <lpjoquino@up.edu.ph>
2025-05-25 08:19:09 +0800

    testing with a button (unsuccessful)

--- main.py
+++ main.py
@@ -2,10 +2,22 @@ from fasthtml.common import *
 
 app,rt = fast_app()
 
-@rt('/')
-def get(): return Div(P('Hello World!'), hx_get="/change")
-
 @rt('/change')
-def get(): return P('Nice to be here!')
+def get(): return P('Testing')
 
-serve()
+@rt('/')
+def get():
+    return Html(
+        Head(
+            Script(src="https://unpkg.com/htmx.org@1.9.10")
+        ),
+        Body(
+            Div(
+                Div(P('One two three'), id="target"),
+                Button("Click Me", hx_get="/change", hx_target="#target", hx_trigger="click")
+            )
+        )
+    )
+
+if __name__ == "__main__":
+    serve()
