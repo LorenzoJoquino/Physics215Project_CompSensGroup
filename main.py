c28303145cba7e8eac5e5613312a62ac658ac639
LorenzoJoquino <lpjoquino@up.edu.ph>
2025-05-26 23:33:15 +0800

    Run some fasthtml

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ from physics215ProjectCode import testFunction
 text = "Joquino,Lorenzo Gabriel, Palad"
 result = testFunction.splitText(text)
 print(result)
+
+
+from fasthtml.common import *
+
+app,rt = fast_app()
+
+@rt('/')
+def get(): return Div(P(f"Hello World! {testFunction.splitText(text)}"), hx_get="/change")
+
+serve()
